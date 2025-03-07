2fdce3642ed3df13fdb82dbabf344ab0993ec610
Night The Fox <kilo.night.prower@gmail.com>
2025-03-07 16:22:35 +0100

    blockstate result in raycast

--- types/raycast.d.ts
+++ types/raycast.d.ts
@@ -7,10 +7,10 @@ declare interface FiguraRaycast
     aabb(startX: number,startY: number,startZ: number,endpos: FiguraVec3,addbs: FiguraAABBs): LuaMultiReturn<[LuaTable,FiguraVec3,string,number]>
     aabb(startpos: FiguraVec3,endX: number,endY: number,endZ: number,addbs: FiguraAABBs): LuaMultiReturn<[LuaTable,FiguraVec3,string,number]>
     aabb(startX: number,startY: number,startZ: number,endX: number,endY: number,endZ: number,addbs: FiguraAABBs): LuaMultiReturn<[LuaTable,FiguraVec3,string,number]>
-    block(startpos: FiguraVec3,endpos: FiguraVec3,blockCastType: BlockRayCastType,fluidCastType: FluidRaycastType): LuaMultiReturn<[LuaTable,FiguraVec3,string]>
-    block(startX: number,startY: number,startZ: number,endpos: FiguraVec3,blockCastType: BlockRayCastType,fluidCastType: FluidRaycastType): LuaMultiReturn<[LuaTable,FiguraVec3,string]>
-    block(startpos: FiguraVec3,endX: number,endY: number,endZ: number,blockCastType: BlockRayCastType,fluidCastType: FluidRaycastType): LuaMultiReturn<[LuaTable,FiguraVec3,string]>
-    block(startX: number,startY: number,startZ: number,endX: number,endY: number,endZ: number,blockCastType: BlockRayCastType,fluidCastType: FluidRaycastType): LuaMultiReturn<[LuaTable,FiguraVec3,string]>
+    block(startpos: FiguraVec3,endpos: FiguraVec3,blockCastType: BlockRayCastType,fluidCastType: FluidRaycastType): LuaMultiReturn<[FiguraBlockState,FiguraVec3,string]>
+    block(startX: number,startY: number,startZ: number,endpos: FiguraVec3,blockCastType: BlockRayCastType,fluidCastType: FluidRaycastType): LuaMultiReturn<[FiguraBlockState,FiguraVec3,string]>
+    block(startpos: FiguraVec3,endX: number,endY: number,endZ: number,blockCastType: BlockRayCastType,fluidCastType: FluidRaycastType): LuaMultiReturn<[FiguraBlockState,FiguraVec3,string]>
+    block(startX: number,startY: number,startZ: number,endX: number,endY: number,endZ: number,blockCastType: BlockRayCastType,fluidCastType: FluidRaycastType): LuaMultiReturn<[FiguraBlockState,FiguraVec3,string]>
     entity(startpos: FiguraVec3,endpos: FiguraVec3,predicate: FiguraRaycastPredicate): LuaMultiReturn<[FiguraEntity,FiguraVec3]>
     entity(startX: number,startY: number,startZ: number,endpos: FiguraVec3,predicate: FiguraRaycastPredicate): LuaMultiReturn<[FiguraEntity,FiguraVec3]>
     entity(startpos: FiguraVec3,endX: number,endY: number,endZ: number,predicate: FiguraRaycastPredicate): LuaMultiReturn<[FiguraEntity,FiguraVec3]>
